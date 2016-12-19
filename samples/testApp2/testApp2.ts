a662c5c5ca90efbe403890f6175f097452874133
Jeff Simon <jeffsim@bluesky.io>
2016-12-18 16:47:59 -0800

    Minor cleanup to testApp2

--- samples/testApp2/testApp2.ts
+++ samples/testApp2/testApp2.ts
@@ -1,15 +1,23 @@
- declare var testMe;
-//$(document).ready(() => {
-  setTimeout(function() {
-//cument.addEventListener("DOMContentLoaded", function(event) {  
-    var v = new Duality.Label();
-    v.test()
-    console.log("i am test app 2");
-    console.log(5);
-    new Duality.TextBox();
-    new Duality.Editor();
-    $("body").html("I am test app 2");
-    var v2 = new PluginTJS.Test();
-    v2.test();
- //   testMe();
+declare var testMe;
+
+setTimeout(function () {
+
+  console.log("i am test app 2");
+  // Create one of the controls from Duality
+  var v = new Duality.Label();
+  v.test()
+
+  // Create another
+  new Duality.Editor();
+
+  // Test that jQuery is working & ambient typings are present
+  $("body").html("I am test app 2");
+
+  // Test local plugin
+  var v2 = new PluginTJS.Test();
+  v2.test();
+
+  // Call the javascript function
+  testMe();
+
 }, 1000);
